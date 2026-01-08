bb35297d5904c578426a8f181c529f536cf8e8ad
kangsol <kangsol0924@gmail.com>
2026-01-08 16:19:25 +0900

    feat: 커스텀 확장자 추가 및 삭제 API 개발

--- src/main/java/com/kangsol/extension_policy/controller/ExtensionController.java
+++ src/main/java/com/kangsol/extension_policy/controller/ExtensionController.java
@@ -1,12 +1,16 @@
 package com.kangsol.extension_policy.controller;
 
+import com.kangsol.extension_policy.dto.CustomExtensionCreateRequest;
 import com.kangsol.extension_policy.dto.ExtensionResponse;
 import com.kangsol.extension_policy.dto.FixedExtensionToggleRequest;
+import com.kangsol.extension_policy.entity.ExtensionPolicy;
 import com.kangsol.extension_policy.service.ExtensionService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/extensions")
@@ -15,6 +19,7 @@ public class ExtensionController {
 
     private final ExtensionService extensionService;
 
+    // 고정 확장자 토글
     @PatchMapping("/fixed/{ext}")
     public ResponseEntity<ExtensionResponse> toggleFixedExtension(
             @PathVariable String ext,
@@ -22,5 +27,25 @@ public class ExtensionController {
     ){
         ExtensionResponse response = extensionService.toggleFixed(ext, request);
         return ResponseEntity.ok(response);
+    }
+
+
+    // 커스텀 확장자 추가
+    @PostMapping("/custom")
+    public ResponseEntity<ExtensionPolicy> addCustom(
+            @Valid @RequestBody CustomExtensionCreateRequest request
+    ){
+        ExtensionPolicy saved = extensionService.addCustomExtension(request);
+
+        return ResponseEntity.created(URI.create("/extensions/custom/" + saved.getId()))
+                .body(saved);
+    }
+
+
+    // 커스텀 확장자 삭제
+    @DeleteMapping("/custom/{id}")
+    public ResponseEntity<Void> deleteCustom(@PathVariable Long id){
+        extensionService.deleteCustomExtension(id);
+        return ResponseEntity.noContent().build();
     }
 }
