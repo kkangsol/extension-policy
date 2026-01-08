4276627ffcf4113be10189d7d5aaf8c0f3b5b874
kangsol <kangsol0924@gmail.com>
2026-01-08 11:44:09 +0900

    setting: initail commit

---
+++ src/main/java/com/kangsol/extension_policy/ExtensionPolicyApplication.java
@@ -0,0 +1,13 @@
+package com.kangsol.extension_policy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensionPolicyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtensionPolicyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kangsol/extension_policy/ExtensionPolicyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kangsol.extension_policy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtensionPolicyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
